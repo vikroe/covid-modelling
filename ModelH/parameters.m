5fa944eeed7b1d819d3bc5cb6845c12c542dd5ab
vikroe <v.koropecky@protonmail.com>
2021-03-12 16:25:39 +0100

    Model H implementation

---
+++ ModelH/parameters.m
@@ -0,0 +1,45 @@
+beta_l = 0.01;
+beta_h = 0.66;
+
+c = 2;
+
+r_beta_l = 0.4;
+r_beta_h = 0.99;
+
+r_c_l = 0.01;
+r_c_h = 0.5;
+
+p_s_l = 0.5;
+p_s_h = 0.99;
+
+p_h = 0.055;
+
+p_j_mu = 0.23;
+p_j_sd = 0.1;
+
+p_d_mu = 0.49;
+p_d_sd = 0.1;
+
+d_E = 6.31;
+sigma = 1 - exp(-1/d_E);
+
+d_Ia = 9.38;
+gammaA = 1-exp(-1/d_Ia);
+
+d_Ip = 3.63;
+psi = 1-exp(-1/d_Ip);
+
+d_Is = 4.8;
+alphaI = 1-exp(-d_Is);
+
+d_Hj = 1.92;
+alphaH = 1-exp(-1/d_Hj);
+
+d_Iz = 1.95;
+gammaZ = 1-exp(-1/d_Iz);
+
+d_J = 4.38;
+alphaJ = 1-exp(-1/d_J);
+
+d_Hr = 3.03;
+gammaH = 1-exp(-1/d_Hr);
